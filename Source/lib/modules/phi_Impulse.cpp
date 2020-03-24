d9ee62c676115e78a308d4d9560be87d342e82ec
alexrodi <alexandre_rodi_1993@hotmail.com>
2020-03-24 13:42:27 +0100

    Better-looking waveform

--- Source/lib/modules/phi_Impulse.cpp
+++ Source/lib/modules/phi_Impulse.cpp
@@ -43,17 +43,21 @@ phi_Impulse::~phi_Impulse()
 
 //==============================================================================
 const float PI = MathConstants<float>::pi;
-static void drawWaveform(juce::Graphics &g, const Rectangle<float> &viewPort, float shape, float decay) {
+static void drawWaveform(juce::Graphics &g, const Rectangle<float> &viewPort, float shape, float decay, Colour colour) {
+    
+    g.setColour(colour);
     
     const int pixelsPerPoint = 2;
+    const float strokeWidth = 2;
     
     const float startX          =  viewPort.getX();
     const float centreY         =  viewPort.getCentreY();
     const float yRange          =  (viewPort.getHeight() * 0.5) - 3;
     const float width           =  viewPort.getWidth();
     const float endX            =  startX+width;
-    const float phaseIncrement  =  (7/width)*pixelsPerPoint; // go up to x=7 (arbitrary value)
     const float shapeValue      =  1.006 - shape;
+    const int aaValue           =  8; // x4 AA
+    const float phaseIncrement  =  ((7/width)*pixelsPerPoint)/aaValue; // go up to x=7 (arbitrary value)
     
     float phase = 0;
     
@@ -63,22 +67,37 @@ static void drawWaveform(juce::Graphics &g, const Rectangle<float> &viewPort, fl
 
     // Add lines to path
     for (int x=startX; x<endX; x += pixelsPerPoint){
-        const float lengthPhase = phase*decay;
-        // This function is not safe for audio routines! it has a singularity at x=PI :)
-        const float y = (lengthPhase==PI) ? 0.f : sin( sin(lengthPhase) / (shapeValue * (lengthPhase-PI)) );
+        float y = 0;
+        for(int i=0; i<aaValue; i++)
+        {
+            const float lengthPhase = phase*decay;
+            // This function is not safe for audio routines! it has a singularity at x=PI :)
+            const float newY = (lengthPhase==PI) ? 0.f : sin( sin(lengthPhase) / (shapeValue * (lengthPhase-PI)) );
+            y += abs(newY);
+            phase += phaseIncrement;
+        }
+        y /= aaValue;
         wavePath.lineTo (x, y * yRange + centreY);
-        phase += phaseIncrement;
     }
+    
+    wavePath.lineTo (endX, centreY);
+    
+    wavePath = wavePath.createPathWithRoundedCorners(60);
 
+    g.setGradientFill(ColourGradient().vertical(colour, centreY+yRange, colour.darker(), centreY));
+    g.fillPath(wavePath);
     g.strokePath (wavePath, PathStrokeType (2.0f));
+    
+    g.setGradientFill(ColourGradient().vertical(colour, centreY-yRange, colour, centreY));
+    wavePath.applyTransform(AffineTransform().verticalFlip(centreY+yRange+strokeWidth*2));
+    g.fillPath(wavePath);
+    g.strokePath (wavePath, PathStrokeType (strokeWidth));
 }
 
 //==============================================================================
 void phi_Impulse::paint (Graphics& g)
 {
-    g.setColour(findColour(Slider::thumbColourId));
-    
-    drawWaveform(g, waveViewPort, shapeDial.getValue(), decayDial.getValue());
+    drawWaveform(g, waveViewPort, shapeDial.getValue(), decayDial.getValue(), findColour(Slider::thumbColourId));
 }
 
 void phi_Impulse::resized()
