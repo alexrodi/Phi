1beec86d98acc6004ea8a27c5e3d334ca2abd66a
alexrodi <alexandre_rodi_1993@hotmail.com>
2020-04-16 01:09:29 +0200

    Include new SliderButton

---
+++ Source/lib/UIobjects/ui_SliderButton.h
@@ -0,0 +1,61 @@
+/*
+  ==============================================================================
+
+    ui_SliderButton.h
+    Created: 15 Apr 2020 12:20:11am
+    Author:  Alexandre Rodrigues
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class ui_SliderButton    : public Button,
+                           public Button::Listener
+{
+public:
+    enum LabelPosition
+    {
+        Above,
+        Below,
+        Left,
+        Right
+    };
+    
+    explicit ui_SliderButton(const String&, LabelPosition = Above);
+    ~ui_SliderButton();
+
+    void paintButton (Graphics&, bool, bool) override;
+    void resized() override;
+    
+    void lookAndFeelChanged() override;
+
+private:
+    const float SLIDER_SIZE = 12;
+    const float SLIDER_WIDTH = SLIDER_SIZE * 2.5;
+    const float SLIDER_CORNER = (SLIDER_SIZE-2) * 0.5;
+    
+    bool shouldDraw = true;
+    
+    LabelPosition labelPosition;
+    Justification labelTextJustification;
+    
+    DrawablePath knob;
+    
+    Rectangle<int> labelBounds;
+    Rectangle<float> sliderBounds;
+    
+    Colour textColour;
+    
+    Rectangle<int> getKnobBounds();
+    
+    void buttonStateChanged(Button*) override;
+    void buttonClicked (Button*) override {};
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ui_SliderButton)
+};
