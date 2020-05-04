2024b12a5b3beb2b9b2e7bb5bd1d3bb1ff73d392
alexrodi <alexandre_rodi_1993@hotmail.com>
2020-05-04 23:29:44 +0200

    New module base classes

---
+++ Source/lib/ModuleProcessor.h
@@ -0,0 +1,66 @@
+/*
+  ==============================================================================
+
+    ModuleProcessor.h
+    Created: 29 Mar 2020 11:38:39am
+    Author:  Alexandre Rodrigues
+
+  ==============================================================================
+*/
+
+#pragma once
+
+///@cond
+#include <JuceHeader.h>
+///@endcond
+
+//==============================================================================
+/*
+ ModuleProcessor - The base class for all modules' DSP implementation
+*/
+class ModuleProcessor    :    public AudioProcessor
+{
+public:
+    /**
+     A Module Processor takes three arguments: Inlet Numbet, Outlet Numbet, juce::AudioProcessorValueTreeState::ParameterLayout...
+     The third and any other following argument allows you to declare any parameters for this processor in the current best practice, via an initializer list:
+     @code
+     MyModuleProcessor::MyModuleProcessor() :
+     ModuleProcessor( 2, 2,
+       std::make_unique<AudioParameterFloat> ("gain", "Gain", NormalisableRange<float> (0.0f, 1.0f), 1.0f),
+       std::make_unique<AudioParameterFloat> ("freq", "Frequency", NormalisableRange<float> (20.0f, 20000.0f), 20.0f)
+     )
+     {...}
+     @endcode
+     */
+    template <typename... Items>
+    ModuleProcessor(int inletNumber, int outletNumber, std::unique_ptr<Items>... paramsToUse) :
+    params( *this, nullptr, "PARAMETERS", AudioProcessorValueTreeState::ParameterLayout(std::move(paramsToUse)...) )
+    {
+        setPlayConfigDetails (inletNumber, outletNumber, getSampleRate(), getBlockSize());
+    }
+    
+    /// Any parameters introduced in the constructor will be available via this object
+    AudioProcessorValueTreeState params;
+    
+    bool hasEditor() const override { return true; }
+    
+private:
+    
+    ///@cond
+    const String getName() const override {return String("");}
+    double getTailLengthSeconds() const override {return 0.0f;}
+    bool acceptsMidi() const override {return false;}
+    bool producesMidi() const override {return false;}
+    int getNumPrograms() override {return 0;}
+    int getCurrentProgram() override {return 0;}
+    void setCurrentProgram (int index) override {}
+    const String getProgramName (int index) override {return String("");}
+    void changeProgramName (int index, const String& newName) override {}
+    void getStateInformation (juce::MemoryBlock& destData) override {}
+    void setStateInformation (const void* data, int sizeInBytes) override {}
+    AudioProcessorEditor* createEditor() override {return nullptr;}
+    ///@endcond
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ModuleProcessor)
+};
---
+++ Source/lib/ModuleUI.h
@@ -0,0 +1,73 @@
+/*
+  ==============================================================================
+
+    ModuleUI.h
+    Created: 21 Apr 2020 7:39:35pm
+    Author:  Alexandre Rodrigues
+
+  ==============================================================================
+*/
+
+#pragma once
+
+///@cond
+#include <JuceHeader.h>
+///@endcond
+#include "Plug.h"
+#include "ModuleProcessor.h"
+
+//==============================================================================
+/*
+ ModuleUI - The base class for all modules' UI view
+*/
+class ModuleUI    : public AudioProcessorEditor
+{
+public:
+    
+    struct Props
+    {
+        const String name; /// This module's name
+        const Array<String> inlets; /// An Array of inlet names to display in the UI (will correspond to the processor's inlets starting from the first)
+        const Array<String> outlets; /// An Array of outlet names to display in the UI (will correspond to the processor's outlets starting from the first)
+        const int width; /// The default width to use when creating this module
+        const int height; /// The default height to use when creating this module
+        const int minimumHeight; /// The minimum height that the module should have to present its UI, any less, and the ModuleBox will collapse to only display the header
+        ModuleProcessor& processor; /// A reference to the ModuleProcessor that spawned this UI
+    };
+    
+    ModuleUI(Props);
+    
+    /** This module's props
+    @see ModuleUI::Props
+     */
+    Props props;
+    
+    /// The module's
+    OwnedArray<Plug> inlets;
+    OwnedArray<Plug> outlets;
+    
+    /// The NodeID belonging to the parent module, this is used by concatenanting it to broadcast this inlet's PlugID
+    AudioProcessorGraph::NodeID nodeID;
+    
+    /**
+     The base-class overrides resized() and exposes wasResized(),
+     containing the actual module's rectangle.
+    */
+    void resized() override;
+    /** The function used by derived classes of ModuleUI to resize their contents,
+    its argument is the rectangle that the content of a derived class should be bound to.
+    */
+    virtual void wasResized(Rectangle<int>) {};
+    
+private:
+    
+    /// Places all plugs in an Array equidistant in a column ( top -> bottom ).
+    void placePlugs(OwnedArray<Plug>&, Rectangle<int>);
+    /**
+     Places the inlets and outlets of this ModuleUI given a rectangle and returns the remaining area
+     The result is used to bound the module contents via wasResized().
+    */
+    const Rectangle<int> placeInletsAndOutlets (Rectangle<int>);
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ModuleUI)
+};
